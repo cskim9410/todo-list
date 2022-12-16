06f4ecaef528c93486d3bdf6d7a5e30d146e1fa3
Kim Chansoo <96669858+cskim9410@users.noreply.github.com>
2022-12-17 02:59:46 +0900

    Update: todo삭제, 완료, 로컬스토리지 구현

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,18 +1,66 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const Todos = () => {
-  const [todos, setTodos] = useState([{ id: 1, text: "대충끄적이기" }]);
+  const [todos, setTodos] = useState([]);
+  const [todoInputValue, setTodoInputValue] = useState("");
+  const isMounted = useRef(false);
+
+  useEffect(() => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (!savedTodos) return;
+    setTodos(savedTodos);
+  }, []);
+
+  useEffect(() => {
+    if (isMounted) {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    } else {
+      isMounted = true;
+    }
+  }, [todos]);
+
+  const clickAddTodo = (text) => {
+    setTodos((state) => {
+      setTodoInputValue("");
+      return [...state, { id: new Date().getTime(), text, isDone: false }];
+    });
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((state) => {
+      return state.filter((todo) => todo.id !== id);
+    });
+  };
+
+  const finishTodo = (id) => {
+    setTodos((state) => {
+      const newTodos = [...state];
+      newTodos.find((todo) => todo.id === id).isDone = !newTodos.find(
+        (todo) => todo.id === id
+      ).isDone;
+      return newTodos;
+    });
+  };
 
   return (
     <div>
-      <input type="text" />
-      <button>+</button>
+      <input
+        type="text"
+        value={todoInputValue}
+        onChange={(e) => setTodoInputValue(e.target.value)}
+      />
+      <button onClick={() => clickAddTodo(todoInputValue)}>+</button>
       <ul>
-        {todos.map(({ id, text }) => {
+        {todos.map(({ id, text, isDone }) => {
           return (
             <li key={id}>
-              <input type="checkbox" />
+              <input
+                type="checkbox"
+                onChange={() => finishTodo(id)}
+                checked={isDone}
+              />
               {text}
+              <button onClick={() => deleteTodo(id)}>X</button>
             </li>
           );
         })}
